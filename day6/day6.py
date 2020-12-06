7fc2ad8c40080b04fa67d48d2cafb9089521e2a7
Lujza Lanyi <lujza.la@gmail.com>
2020-12-06 18:15:55 +0100

    part 2 solved day 6

--- day6/day6.py
+++ day6/day6.py
@@ -1,29 +1,16 @@
 
-with open("example.txt", 'r') as inpt:
+with open("input.txt", 'r') as inpt:
     line = inpt.read().split("\n\n")
 
     count = sum([len(set([c for c in group if not c.isspace()])) for group in line])
+    print("Part 1:")
     print(count)
 
-    print([group for group in line])
 
-    interesct_count = [len([person for person in group.split('\n')]) for group in line]
+groups = [[set(person) for person in group.splitlines()] for group in line]
 
-    intersect_cnt = 0
-    checked_c = []
-    for group in line:
-        persons = group.split('\n')
-        for person in persons:
-            for c in person:
-                if c.isspace():
-                    continue
-                if c in checked_c:
-                    continue
-                checked_c.append(c)
-                if person.find(c) != -1:
-                    
-                if num_c == len(persons):
-                    intersect_cnt = intersect_cnt + 1
-        checked_c = []
+intersections = sum(len(set.intersection(*group)) for group in groups)
 
-    print(intersect_cnt)
+print("Part 2: ")
+print(intersections)
+
