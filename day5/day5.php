cee3987775023ae18dda49eb316dfc68e936d449
Lujza Lanyi <lujza.la@gmail.com>
2020-12-06 19:24:43 +0100

    implemented parse_row()

--- day5/day5.php
+++ day5/day5.php
@@ -1,2 +1,50 @@
 <?php
 
+// F front
+// B back
+// L left
+// R right
+
+function parse_row($bcode) {
+    
+    $rcodes = substr($bcode, 0, 7);
+
+    var_dump($rcodes);
+
+    $range = array(0,127);
+
+    foreach (str_split($rcodes) as $char) {
+        if($char == 'F') {
+            $range[1] = floor(($range[0]+$range[1])/2);
+        } else {
+            $range[0] = ceil(($range[0]+$range[1])/2);
+        }
+    }
+    return $range;
+}
+
+var_dump(parse_row("FBFBBFFRLR"));
+
+function parse_column($bcode) {
+    return 0;
+}
+
+$myfile = fopen("input.txt", "r") or die("Unable to open file!");
+$lines = fread($myfile, filesize("input.txt"));
+fclose($myfile);
+
+$bcodes = explode("\n", $lines);
+
+//var_dump($bcodes);
+
+$seat_ids = array();
+
+foreach ($bcodes as $bcode) {
+    $row = parse_row($bcode);
+    $column = parse_column($bcode);
+
+    $seat_id = $row*8 + $column;
+    array_push($seat_ids, $seat_id);
+}
+
+echo max($seat_ids);
