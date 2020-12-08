f084f310c8b8ccb21995c9db978d1fba3a5e9943
noxeee <lujza.la@gmail.com>
2020-12-08 18:35:47 +0100

    added instructions and input data

---
+++ day8/day8.scala
@@ -0,0 +1,15 @@
+import scala.io.Source
+
+// reading file in Scala
+val filename = "input_day8.txt"
+val lines = Source.fromFile(filename).getLines.toArray
+
+def read(input:Array[String]){
+    
+    
+    input match {
+        case "nop" <= break
+        case "acc" <= 
+        case "jmp" =>
+    }
+}
